9e60cc1bbb10a3f0b4d8cacdf45ea8d148df5cb5
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-09-16 15:43:43 +0530

    Tabbar customized

--- app/navigators/TopTabNavigator.tsx
+++ app/navigators/TopTabNavigator.tsx
@@ -7,7 +7,7 @@ import { MarketOverviewScreen, WatchlistScreen, GuidantsNewsScreen } from "app/s
 import tw from "app/lib/tw"
 
 export type TopTabNavigatorParamList = {
-  MarketOverview: undefined
+  "Market Overview": undefined
   Watchlist: undefined
   GuidantsNews: undefined
 }
@@ -20,11 +20,14 @@ export const TopTabNavigator = () => {
     tabBarStyle: tw`bg-primary`,
     tabBarActiveTintColor: tw.color("white"),
     tabBarInactiveTintColor: tw.color("secondary"),
+    tabBarLabelStyle: tw`capitalize text-base`,
+    tabBarScrollEnabled: true,
+    tabBarIndicatorStyle: tw`bg-accent`
   }
 
   return (
     <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen name="MarketOverview" component={MarketOverviewScreen} />
+      <Tab.Screen name="Market Overview" component={MarketOverviewScreen} />
       <Tab.Screen name="Watchlist" component={WatchlistScreen} />
       <Tab.Screen name="GuidantsNews" component={GuidantsNewsScreen} />
     </Tab.Navigator>
